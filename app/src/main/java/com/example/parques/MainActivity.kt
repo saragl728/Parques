684ef1e9d9e29155984551ea53c2f238f80256dc
Usuario <saragl728@gmail.com>
2023-10-13 16:33:20 +0200

    Hago que el texto por defecto de ciertas partes ahora sea solo una pista

--- app/src/main/java/com/example/parques/MainActivity.kt
+++ app/src/main/java/com/example/parques/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.parques
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.parques.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,18 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         var binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //se debería hacer que el botón añadiera notificaciones al registro
+        binding.bGuardar.setOnClickListener {
+            /*String salida = "Datos del parque"
+            salida += binding.nombre.text
+            salida += binding.descripcion.text
+            salida += binding.telefono.text
+            salida += binding.web.text
+            añadir los datos del horario
+            comprobar los checkbox
+            */
+            Log.d("Guardado", "salida")
+        }
     }
 }
