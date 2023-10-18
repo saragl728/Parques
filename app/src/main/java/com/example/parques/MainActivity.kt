c3a9fd5b93af3255bd5af3c1bc5b3f1882593ff6
Usuario <saragl728@gmail.com>
2023-10-18 08:26:51 +0200

    Añado los Log

--- app/src/main/java/com/example/parques/MainActivity.kt
+++ app/src/main/java/com/example/parques/MainActivity.kt
@@ -15,42 +15,51 @@ class MainActivity : AppCompatActivity() {
 
         //se debería hacer que el botón añadiera notificaciones al registro
         binding.bGuardar.setOnClickListener {
-            /*String salida = "Datos del parque"
-            salida += binding.nombre.text
-            salida += binding.descripcion.text
-            salida += binding.telefono.text
-            salida += binding.web.text
-            añadir los datos del horario
-            */
+
+            //nombre
+            Log.d("Nombre", binding.nombre.text.toString())
+
+            //teléfono
+            Log.d("Teléfono", binding.telefono.text.toString())
+
+            //descripción
+            Log.d("Descripción", binding.descripcion.text.toString())
+
+            //web
+            Log.d("Web", binding.web.text.toString())
 
             //se comprueban los checkboxs
             if (binding.cbMascotas.isChecked){
-
+                Log.d("Mascotas", binding.cbMascotas.text.toString())
             }
 
             if (binding.cbDeportes.isChecked){
 
+                Log.d("Deportes", binding.cbDeportes.text.toString())
             }
 
             if (binding.cbRestaurante.isChecked){
-
+                Log.d("Restaurantes", binding.cbDeportes.text.toString())
             }
 
             if (binding.cbParqueInf.isChecked){
-
+                Log.d("Parque infantil", binding.cbParqueInf.text.toString())
             }
 
             //en caso de que en las opciones de hora estuviera 24 h, se cambiaría el otro
-            if (binding.hApertura.selectedItem.equals("24 h")){
-
+            if (binding.hApertura.selectedItem.toString() == "24 h"){
+                //binding.hCierre.selectedItem = "24 h"
             }
 
-            if (binding.hCierre.selectedItem.equals("24 h")){
-
+            if (binding.hCierre.selectedItem.toString() == "24 h"){
+                //binding.hApertura.selectedItem = "24 h"
             }
 
+            //se muestran las horas de cierre y apertura
+            Log.d("Hora apertura", binding.hApertura.selectedItem.toString())
+            Log.d("Hora de cierre", binding.hCierre.selectedItem.toString())
 
-            Log.d("Guardado del parque", "salida")
+
         }
     }
 }
